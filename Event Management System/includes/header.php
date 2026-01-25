17dfff47ae62255c1b26512719c375914cc289e6
Shambhu Sapkota <sapkotashambhu65@gmail.com>
2026-01-25 21:55:38 +0545

    update the header
    for the connection of css and js to the respectively

--- Event Management System/includes/header.php
+++ Event Management System/includes/header.php
@@ -1,15 +1,14 @@
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Event Management System</title>
+
     <!-- CSS -->
-    <link rel="stylesheet" href="/Event Management System/assets/css/style.css">
+    <link rel="stylesheet" href="/COURSE_WORK/Event%20Management%20System/assets/css/style.css">
 
     <!-- JS -->
-    <script src="/Event Management System/assets/js/ajax.js" defer></script>
-    <script src="/Event Management System/assets/js/validation.js" defer></script>
+    <script src="/COURSE_WORK/Event%20Management%20System/assets/js/ajax.js" defer></script>
 </head>
 <body>
 <h1>Event Management System</h1>
