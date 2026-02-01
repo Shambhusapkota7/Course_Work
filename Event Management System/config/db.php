d6130e5ebdc9429001935b8e72af7ffbc4d86f33
Shambhu Sapkota <sapkotashambhu65@gmail.com>
2026-02-01 14:52:47 +0545

    changes made in db

--- Event Management System/config/db.php
+++ Event Management System/config/db.php
@@ -1,16 +1,19 @@
 <?php
-$host = "localhost";
+$host = "127.0.0.1";
 $db   = "np02cs4a240060";
 $user = "np02cs4a240060";
 $pass = "LwK14Mh4Aw";
 
 try {
     $pdo = new PDO(
-        "mysql:host=$host;dbname=$db;charset=UTF8",
+        "mysql:host=$host;dbname=$db;charset=utf8",
         $user,
         $pass,
-        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]
     );
 } catch (PDOException $e) {
-    die("Database connection failed");
+    die("Database connection failed: " . $e->getMessage());
 }
